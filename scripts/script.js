96ad7e36ebb64aad2f3bd94ea81c9d1295ba6ab0
root <jsharman@hotmail.com.au>
2022-03-19 16:32:39 +1100

    m

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function butclick() {
+    console.log("here")
+    boxElem = document.querySelectorAll('.box')
+    boxElem[0].style.left="400px"
+
+}
+function startPage() {
+    console.log("here")
+    boxElem = document.querySelectorAll('.box')
+    boxElem[0].style.left="100%"
+}
+startPage()
